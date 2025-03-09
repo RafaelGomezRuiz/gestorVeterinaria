950f34cce6a18f5c131d23c67919f497f07d958e
Rafael Gomez <rafaelogomezr@gmail.com>
2025-03-08 20:07:54 -0400

    solving problem with ,gitignore

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Formulario from './components/Formulario.vue';
+import Header from './components/Header.vue';
+</script>
+
+<template>
+    <div class="container mx-auto mt-20">
+        <Header />
+        <div class="md:flex mt-12">
+            <Formulario/>
+        </div>
+    </div>
+</template>
+
+ 
