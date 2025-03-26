a2c8c067b4a1522cc50460a44626c90e6e358215
Rafael Gomez <rafaelogomezr@gmail.com>
2025-03-26 12:40:02 -0400

    ya realiza el crud con el local storage

---
+++ src/components/Alerta.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {computed} from 'vue';
+
+const props= defineProps({
+    alerta:{
+        type: Object,
+        required:true
+    }
+})
+
+const isError = computed(()=>{
+    console.log("sawd ",  props.alerta.tipo);
+    return props.alerta.tipo === 'error'
+})
+</script>
+
+<template>
+    <div 
+        :class="[isError ? ['bg-red-500', 'cursor-not-allowed'] : 'bg-green-500' ,'cursor-not-allowed']" 
+        class="text-white text-center p-3 uppercase font-boldmn-3 rounded-md">
+        {{alerta.mensaje}}
+    </div>
+</template>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,4 +1,68 @@
 <script setup>
+import { reactive, computed } from 'vue';
+import Alerta from './Alerta.vue';
+
+const alerta=reactive({
+    tipo:'',
+    mensaje:''
+})
+
+const validar =()=>{
+    // if([pacientes.nombreMascota , pacientes.nombrePropietario].includes('')){
+    //     console.log("vaciosss");
+    // }
+    console.log("modal ", Object.values(props).includes(''));
+    if(Object.values(props).includes('')){
+        alerta.mensaje="Todos los campos son obligatorios"
+        alerta.tipo = 'error' 
+        return;
+    }; 
+    emit('guardar-paciente')
+    alerta.mensaje="Exitoso"
+    alerta.tipo = 'success' 
+    
+    console.log("alerta ", alerta);
+    setTimeout(()=>{
+        Object.assign(alerta, {
+            tipo:'',
+            mensaje:''
+        })
+    }, 3000)
+}
+
+const emit = defineEmits(['update:nombre', 'update:propietario', 'update:correo', 'update:alta', 'update:sintomas', 'guardar-paciente']);
+
+const props = defineProps({
+    nombre: {
+        type: String,
+        required: true
+    },
+    propietario: {
+        type: String,
+        required: true
+    },
+    correo: {
+        type: String,
+        required: true
+    },
+    alta: {
+        type: String,
+        required: true
+    },
+    sintomas: {
+        type: String,
+        required: true
+    },
+    id:{
+        type: [String, null],
+        required: true
+    }
+});
+
+const editanto = computed(()=>{
+    return props.id;
+})
+
 </script>
 
 <template>
@@ -8,17 +72,87 @@
             Añade pacientes y
             <span class="text-indigo-600">Adminístralos</span>
         </p>
-        <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
-            <label 
-                for="mascota"
-                class="block text-gray-700 uppercase font-bold">
-                Nombre mascota
-            </label>
+        <Alerta v-if="alerta.mensaje" :alerta="alerta"/>
+        <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+            @submit.prevent="validar">
+            <div class="mb-5">
+                <label 
+                    for="mascota"
+                    class="block text-gray-700 uppercase font-bold">
+                    Nombre mascota
+                </label>
+                <input
+                    id="mascota"
+                    type="text"
+                    placeholder="Nombre de la mascota"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    :value="nombre"
+                    @input="$emit('update:nombre', $event.target.value)"
+                />
+            </div>
+            <div class="mb-5">
+                <label 
+                    for="propietario"
+                    class="block text-gray-700 uppercase font-bold">
+                    Nombre propietario
+                </label>
+                <input
+                    id="propietario"
+                    type="text"
+                    placeholder="Nombre del propietario"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    :value="propietario"
+                    @input="$emit('update:propietario', $event.target.value)"
+                />
+            </div>
+            <div class="mb-5">
+                <label 
+                    for="correo"
+                    class="block text-gray-700 uppercase font-bold">
+                    Correo
+                </label>
+                <input
+                    id="correo"
+                    type="email"
+                    placeholder="Ingresa tu correo"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    :value="correo"
+                    @input="$emit('update:correo', $event.target.value)"
+                />
+            </div>
+            <div class="mb-5">
+                <label 
+                    for="alta"
+                    class="block text-gray-700 uppercase font-bold">
+                    Alta
+                </label>
+                <input
+                    id="alta"
+                    type="date"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    :value="alta"
+                    @input="$emit('update:alta', $event.target.value)"
+                />
+            </div>
+            <div class="mb-5">
+                <label 
+                    for="sintomas"
+                    class="block text-gray-700 uppercase font-bold">
+                    Síntomas
+                </label>
+                <textarea
+                    id="sintomas"
+                    placeholder="Describe los síntomas"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
+                    :value="sintomas"
+                    @input="$emit('update:sintomas', $event.target.value)"
+                />
+            </div>
             <input
-                id="mascota"
-                type="text"
-                placeholder="Nombre de la mascota"
-                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                type="submit"
+                class="bg-indigo-600 w-full p-3 text-white uppercase font-bold 
+                hover:bg-indigo-700 cursor-pointer transition-colors"
+                :value="editanto ? 'Guardar paciente' : 'Registrar paciente'"
             />
         </form>
     </div>
